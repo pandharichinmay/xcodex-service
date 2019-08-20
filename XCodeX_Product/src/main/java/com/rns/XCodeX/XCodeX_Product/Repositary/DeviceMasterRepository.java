336bfa11621939b57bf2bee221208453703d3e9a
Chinmay <pandharichinmay@gmail.com>
2019-08-20 16:45:16 +0530

    commit

---
+++ XCodeX_Product/src/main/java/com/rns/XCodeX/XCodeX_Product/Repositary/DeviceMasterRepository.java
@@ -0,0 +1,9 @@
+package com.rns.XCodeX.XCodeX_Product.Repositary;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.rns.XCodeX.XCodeX_Product.model.DeviceMaster;
+
+public interface DeviceMasterRepository extends CrudRepository<DeviceMaster, Long>{
+
+}
---
+++ XCodeX_Product/src/main/java/com/rns/XCodeX/XCodeX_Product/model/DeviceMaster.java
@@ -0,0 +1,65 @@
+package com.rns.XCodeX.XCodeX_Product.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.websocket.Decoder.Text;
+
+@Table(name = "device_ids")
+@Entity
+public class DeviceMaster implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "iddevice", unique = true, nullable = false)
+	private Long iddevice;
+	@ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", referencedColumnName = "id", insertable = false, updatable = false)
+	private UserMaster idUser;
+	@Column(name = "created_date")
+	private Date createdDate;
+	@Column(name = "device_id")
+	private String deviceId;
+
+	public String getDeviceId() {
+		return deviceId;
+	}
+
+	public void setDeviceId(String deviceId) {
+		this.deviceId = deviceId;
+	}
+
+	public Long getIddevice() {
+		return iddevice;
+	}
+
+	public void setIddevice(Long iddevice) {
+		this.iddevice = iddevice;
+	}
+
+	public UserMaster getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(UserMaster idUser) {
+		this.idUser = idUser;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+}
